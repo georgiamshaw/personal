b30d1e870f6023f835ce516e09bc8def444e079c
georgia.shaw <georgia@cranbrookw4.com>
2019-08-30 11:11:50 +0100

    write public route in router.js

--- src/router.js
+++ src/router.js
@@ -14,8 +14,25 @@ const router = (request, response) => {
         response.end(file);
       }
     });
+  } else if (request.url.startsWith('public')) {
+      const filePath = path.join(__dirname, '..', request.url);
+      const extension = path.extname(request.url);
+      const extensionType = {
+        '.html': 'text/html',
+        '.css': 'text/css',
+        '.js': 'application/ajavscript',
+        '.ico': 'image/x-icon'
+      };
+      fs.readFile(filePath, (error, file) => {
+        if (error) {
+          response.writeHead(404, { 'content-type': 'text/html'});
+          response.end('404: page not found');
+        } else {
+          response.writeHead(200, {'content-type': extensionType[extension]});
+          response.end(file);
+        }
+      });
   }
-
 }
 
 module.exports = router;

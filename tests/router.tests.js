97f0fd5fddef6e21584c01e520df9827442058d0
georgia.shaw <georgia@cranbrookw4.com>
2019-08-30 10:32:06 +0100

    wrote a test to check that the home route works

--- tests/router.tests.js
+++ tests/router.tests.js
@@ -6,3 +6,22 @@ test('tests are running', t => {
   t.equal(1, 1, 'our tests are running');
   t.end();
 });
+
+test('home route works', t => {
+  supertest(router)
+  .get('/')
+  .expect(200)
+  .end((error, response) => {
+    t.error(error, 'an error has been thrown')
+    t.equal(
+      response.statusCode,
+      200,
+      'home has a statuscode of 200');
+    t.equal(
+      response.headers['content-type'],
+      'text/html',
+      'home route has correct content type - html'
+    );
+    t.end();
+  });
+});

dbcce8d55afabbf40d4566d32c8b2444ff2bcb8f
georgia.shaw <georgia@cranbrookw4.com>
2019-10-20 19:16:57 +0100

    create handlers for different endpoints and add to router

--- src/router.js
+++ src/router.js
@@ -1,15 +1,29 @@
-const fs = require('fs');
-const url = require('url');
-const path = require('path');
-const { handleHome, handlePublic } = require('./handler.js');
+const fs = require("fs");
+const url = require("url");
+const path = require("path");
+const {
+  handleHome,
+  handlePublic,
+  handleAbout,
+  handleTech,
+  handleContact,
+  handleProjects
+} = require("./handler.js");
 
 const router = (request, response) => {
-  if (request.url === '/') {
+  if (request.url === "/") {
     handleHome(request, response);
-  }
-  else if (request.url.startsWith('/public')) {
+  } else if (request.url.startsWith("/public")) {
     handlePublic(request, response);
-  };
-}
+  } else if (request.url.startsWith("/about")) {
+    handleAbout(request, response);
+  } else if (request.url.startsWith("/projects")) {
+    handleProjects(request, response);
+  } else if (request.url.startsWith("/tech-stack")) {
+    handleTech(request, response);
+  } else if (request.url.startsWith("/contact")) {
+    handleContact(request, response);
+  }
+};
 
 module.exports = router;
